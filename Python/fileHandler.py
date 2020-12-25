ac7f79604d011b58ef0dd71583dd93ebf2dbedf2
tarek99samy <tarek.saad99@eng-st.cu.edu.eg>
2020-12-25 14:32:18 +0200

    fix runtime and add finish time initialized with 0

--- Python/fileHandler.py
+++ Python/fileHandler.py
@@ -7,5 +7,7 @@ def fileHandler(path="../code/processes.txt"):
             continue
         x = list(map(int, line.split("\t")))
         processes.append({"id": x[0], "start": x[1],
-                          "finish": x[2]+x[1], "priority": x[3]})
+                          "runtime": x[2], "finish": 0, "priority": x[3]})
     return processes
+
+# print(fileHandler())
