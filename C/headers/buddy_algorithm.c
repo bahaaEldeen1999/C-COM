5b22c40da21a78eac9759c2e1626e6255f04223d
EmanOthman21 <eothman21@gmail.com>
2021-01-14 00:49:05 +0200

    Add deallocate function

--- C/headers/buddy_algorithm.c
+++ C/headers/buddy_algorithm.c
@@ -34,6 +34,7 @@ void initializeFreeList() {
     freeArr.free[9].lastIndex +=1;
 }
 
+// -------------------------------- Allocate a memory for a processes with fixed size----------------------------
 struct pair allocate(int requiredSize) {
 
     //Initilaize the pair with (-1,-1) (not found answer)
@@ -128,10 +129,105 @@ struct pair allocate(int requiredSize) {
     return result;
 }
 
+// -------------------------------- Free process memory------------------------------------
+void deallocate(int startIndex, int endIndex) {
+
+    int blockSize = endIndex - startIndex +1;
+    int n = ceil(log(blockSize)/log(2))-1;
+
+    struct pair addedBlock = {.start = startIndex, .end = endIndex };
+
+    freeArr.free[n].pairList[freeArr.free[n].lastIndex] = addedBlock;
+    freeArr.free[n].lastIndex +=1;
+
+    for(int i = startIndex; i<=endIndex;i++) {
+        memoryArr[i] = '0';
+    }
+    printf("Memory freeed from %d to %d \n", startIndex,endIndex);
+    // Merge two blocks if possible
+    int buddyNum = startIndex / blockSize;
+    int buddyStartAddress;
+
+
+
+    // Calculate the buddy address
+    if(buddyNum %2 ==0) {
+
+        //   --------------------------
+        //   |Added block| Buddy block|
+        //   --------------------------
+        buddyStartAddress = startIndex + blockSize;
+    } else {
+
+        //   --------------------------
+        //   |Buddy block| Added block|
+        //   --------------------------
+        buddyStartAddress = startIndex - blockSize;
+    }
+
+    // Search for the buddy
+    for(int i=0;i<freeArr.free[n].lastIndex;i++) {
+
+        // Buddy is found
+        if(freeArr.free[n].pairList[i].start == buddyStartAddress) {
+            //printf("buddy found at %d  me at %d\n" , i,freeArr.free[n].lastIndex);
+            // Calculate the new block start & end depending on buddy position
+            struct pair newPair;
+            if(buddyNum %2 == 0) {
+
+                newPair.start = startIndex;
+                newPair.end = startIndex + 2 * blockSize -1;
+
+                // printf("buddy left to me\n");
+                // printf("new pair %d %d \n", newPair.start, newPair.end);
+
+            } else {
+
+                newPair.start = buddyStartAddress;
+                newPair.end = buddyStartAddress + 2*blockSize;
+
+            }
+
+            // printf("n+1 before \n");
+            // for(int j =0;j<freeArr.free[n+1].lastIndex;j++) {
+            //     printf("%d %d \n", freeArr.free[n+1].pairList[j].start,freeArr.free[n+1].pairList[j].end);
+            // }
+            // Store the new block on the free list of size n+1
+            freeArr.free[n+1].pairList[freeArr.free[n+1].lastIndex] = newPair;
+            freeArr.free[n+1].lastIndex +=1;
+
+
+            // printf("n+1 after \n");
+            // for(int j =0;j<freeArr.free[n+1].lastIndex;j++) {
+            // printf("%d %d \n", freeArr.free[n+1].pairList[j].start,freeArr.free[n+1].pairList[j].end);
+            // }
+        }
+
+        // printf("n before \n");
+        // for(int j =0;j<freeArr.free[n].lastIndex;j++) {
+        //     printf("%d %d \n", freeArr.free[n].pairList[j].start,freeArr.free[n].pairList[j].end);
+        // }
+
+        // Remove the 2 blocks
+        freeArr.free[n].lastIndex -=1;
+
+        freeArr.free[n].pairList[i] = freeArr.free[n].pairList[freeArr.free[n].lastIndex-1];
+        freeArr.free[n].lastIndex -=1;
+
+        // printf("n after \n");
+        // for(int j =0;j<freeArr.free[n].lastIndex;j++) {
+        //     printf("%d %d \n", freeArr.free[n].pairList[j].start,freeArr.free[n].pairList[j].end);
+        // }
+        break;
+    }
+
+}
+
 int main() {
     initializeFreeList();
     struct pair temp =allocate(16);
-    temp = allocate(8);
-    temp =allocate(64);
-    temp =allocate(16);
+    deallocate(temp.start,temp.end);
+    temp = allocate(16);
+    // temp =allocate(64);
+    // temp =allocate(16);
 }
